71e339198d74f18440a915f475b50090de273d30
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-04 14:56:00 +0900

    StockListにCSSグリッドを導入

--- client/src/components/StockList/index.tsx
+++ client/src/components/StockList/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Stock from '../Stock'
+import { Container } from './style'
 
 const StockList: React.FC = () => {
-  const stockProps = {
-    name: '製品A',
-  }
-  return <Stock {...stockProps} />
+  const stockProps = [
+    { name: '在庫AAA' },
+    { name: '在庫BBB' },
+    { name: '在庫CCC' },
+    { name: '在庫ABC' },
+    { name: '在庫BCA' },
+    { name: '在庫CAB' },
+    { name: '在庫BBC' },
+    { name: '在庫CCA' },
+  ]
+
+  const stockList = stockProps.map((prop) => {
+    return <Stock {...prop} />
+  })
+  return <Container>{stockList}</Container>
 }
 
 export default StockList
---
+++ client/src/components/StockList/style.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 120px 120px 1fr;
+`

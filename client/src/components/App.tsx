414b4b47bf7ef8d5b7b4ac7fdc872c45b39ec04c
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-04 16:46:25 +0900

    API(stocks)へのアクセス

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Layout } from 'antd'
+import axios from 'axios'
 import 'antd/dist/antd.css'
 import StockList from './StockList'
-import { Layout } from 'antd'
 import SearchForm from './SearchForm'
 
 const { Content } = Layout
 
 function App() {
+  const [stocks, setStocks] = useState([])
+
+  useEffect(() => {
+    const getStocks = async () => {
+      const response = await axios.get('http://localhost:3000/api/v1/stocks/')
+
+      console.log('response')
+      console.log(response)
+    }
+
+    getStocks()
+  }, [])
+
   return (
     <div className="App">
       <Content>

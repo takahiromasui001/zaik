8e710165c00517e59dd9e29b671257124c3b5825
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-04 18:50:16 +0900

    不要コードの削除

--- client/src/components/StockList/index.tsx
+++ client/src/components/StockList/index.tsx
@@ -7,17 +7,6 @@ type TStockList = { stocks: TStock[] }
 
 const StockList: React.FC<TStockList> = (props) => {
   const { stocks } = props
-  
-  const stockProps = [
-    { name: '在庫AAA' },
-    { name: '在庫BBB' },
-    { name: '在庫CCC' },
-    { name: '在庫ABC' },
-    { name: '在庫BCA' },
-    { name: '在庫CAB' },
-    { name: '在庫BBC' },
-    { name: '在庫CCA' },
-  ]
 
   const stockList = stocks.map((prop: TStock) => {
     return <Stock {...prop} key={prop.id}/>

6d51d4d2417a8bd16b71bda1a6e83f170dbaf9c9
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-06 12:03:04 +0900

    StockDetailでのデータ取得

--- client/src/components/Stock/StockDetail/index.tsx
+++ client/src/components/Stock/StockDetail/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+type TStock = {
+  name: string
+}
 
 const StockDetail = () => {
   let { id } = useParams()
+  const [stock, setStock] = useState({} as TStock)
 
-  return <div>{`StockDetail No.${id}`}</div>
+  useEffect(() => {
+    const getStocks = async () => {
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/stocks/${id}`
+      )
+
+      setStock(response.data)
+    }
+
+    getStocks()
+  }, [])
+
+  return (
+    <div>
+      {`StockDetail No.${id}`}
+      <div>{stock.name}</div>
+    </div>
+  )
 }
 
 export default StockDetail

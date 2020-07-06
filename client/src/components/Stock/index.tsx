7f702f02aa07ba18bf72fbcf05cfe733a2932a21
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-06 11:26:31 +0900

    react-router導入

--- client/src/components/Stock/index.tsx
+++ client/src/components/Stock/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import 'antd/dist/antd.css'
 import StockList from './StockList'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const Stock = () => {
-  return <StockList />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <StockList />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default Stock

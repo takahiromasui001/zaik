e649b940338222016e718083c86973bdc8f9fe5d
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-06 13:12:23 +0900

    StockDetail作成

--- client/src/components/Stock/StockDetail/index.tsx
+++ client/src/components/Stock/StockDetail/index.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { Descriptions, Empty } from 'antd'
+import { ImgContainer } from './style'
 
 type TStock = {
   name: string
+  colorNumber: string
+  manufacturingDate: string
+  quantity: number
+  used: boolean
+  storehouse: string
+  file: any
 }
 
 const StockDetail = () => {
@@ -22,11 +30,42 @@ const StockDetail = () => {
     getStocks()
   }, [])
 
+  const dataURLFile = (file: any) => `data:image/png;base64,${file}`
+  const img = stock.file ? (
+    <img
+      src={dataURLFile(stock.file)}
+      style={{ margin: '10px 0', maxHeight: 320 }}
+      alt="stock images"
+    />
+  ) : (
+    <Empty
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+      style={{ margin: '10px 0' }}
+      imageStyle={{ height: 70 }}
+    />
+  )
+
   return (
-    <div>
-      {`StockDetail No.${id}`}
-      <div>{stock.name}</div>
-    </div>
+    <>
+      <ImgContainer>{img}</ImgContainer>
+
+      <Descriptions bordered>
+        <Descriptions.Item label="品名">{stock.name}</Descriptions.Item>
+        <Descriptions.Item label="色番号">
+          {stock.colorNumber}
+        </Descriptions.Item>
+        <Descriptions.Item label="残量">{stock.quantity}</Descriptions.Item>
+        <Descriptions.Item label="製造年月日">
+          {stock.manufacturingDate}
+        </Descriptions.Item>
+        <Descriptions.Item label="新品・中古">
+          {stock.used ? '中古' : '新品'}
+        </Descriptions.Item>
+        <Descriptions.Item label="保管場所">
+          {stock.storehouse}
+        </Descriptions.Item>
+      </Descriptions>
+    </>
   )
 }
 
---
+++ client/src/components/Stock/StockDetail/style.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const ImgContainer = styled.div`
+  text-align: center;
+`

645d03790aaf6bc7c981a9d4680fa34a9cfc9fe9
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-20 10:49:04 +0900

    ログイン画面(見た目だけ)

--- client/src/components/Authentication/Login/index.tsx
+++ client/src/components/Authentication/Login/index.tsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
 
 const Login = () => {
-  return <div>Login</div>
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  }
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
+  return (
+    <div style={{ padding: 24 }}>
+      <h2 style={{ textAlign: 'center', marginBottom: 20 }}>
+        在庫管理システム(仮称)にログイン
+      </h2>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          // label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input placeholder={'ユーザ名'} />
+        </Form.Item>
+
+        <Form.Item
+          // label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password placeholder={'パスワード'} />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
 
 export default Login

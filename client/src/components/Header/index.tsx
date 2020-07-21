4229f47eed784fda1ff1db7d082bec045c190726
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-21 18:33:15 +0900

    ヘッダにサブメニュー選択用ドロップダウンを追加

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { HeaderContainer, Title } from './style'
 import { UnorderedListOutlined } from '@ant-design/icons'
+import { Dropdown, Menu } from 'antd'
+import { useHistory } from 'react-router-dom'
 
 const Header: React.FC = () => {
+  const history = useHistory()
+
+  function handleButtonClick(e: any) {
+    console.log('click left button')
+  }
+
+  const handleMenuClick = (e: any) => {
+    if (e.key === '2') {
+      history.push('/login')
+    }
+  }
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="1">管理者ページ</Menu.Item>
+      <Menu.Item key="2">ログアウト</Menu.Item>
+    </Menu>
+  )
+
   return (
     <HeaderContainer>
-      <Title>在庫管理システム</Title>
-      <UnorderedListOutlined
-        style={{ fontSize: '25px', marginLeft: 'auto', paddingRight: 13 }}
+      <Title onClick={() => history.push('/stocks')}>在庫管理システム</Title>
+      <Dropdown.Button
+        overlay={menu}
+        onClick={handleButtonClick}
+        placement="bottomCenter"
+        style={{ border: '0px', marginLeft: 'auto', paddingRight: 13 }}
+        icon={<UnorderedListOutlined style={{ fontSize: '25px' }} />}
       />
     </HeaderContainer>
   )

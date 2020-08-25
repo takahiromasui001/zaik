b637e5013af1cdf1270c2da6c38f1e237f775673
Takahiro Masui <takahiro.masui@luxiar.com>
2020-08-25 09:39:30 +0900

    stock spec 'GET/api/v1/stocks'

--- api/spec/requests/api/stocks_spec.rb
+++ api/spec/requests/api/stocks_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::StocksController, type: :request do
+  def create_first_stock
+    storehouse = create(:storehouse)
+    stock = create(:stock, storehouse: storehouse)
+    [stock, storehouse]
+  end
+
   describe 'GET	/api/v1/stocks' do
     context '未ログインの場合' do
       it 'HTTPステータスが401であること' do
-        storehouse = create(:storehouse)
-        stock = create(:stock, storehouse: storehouse)
-  
+        create_first_stock
+
         get api_v1_stocks_path
-  
+
         expect(response).to have_http_status 401
       end
     end
@@ -16,17 +21,53 @@ RSpec.describe Api::V1::StocksController, type: :request do
     context 'ログイン済みの場合' do
       it 'HTTPステータスが200 OKであること' do
         login
-
         storehouse = create(:storehouse)
-        stock = create(:stock, storehouse: storehouse)
-  
+        3.times { |n| create(:stock, name: "stock#{n}", storehouse: storehouse) }
+
         get api_v1_stocks_path
-        result = JSON.parse(response.body)
-        pp result
-  
+        result = JSON.parse(response.body).map { |n| n.symbolize_keys }
+
+        expected = [
+          { name: 'stock0', file: nil},
+          { name: 'stock1', file: nil},
+          { name: 'stock2', file: nil}
+        ]
+
         expect(response).to have_http_status 200
-        expect(result.size).to eq 1
+        expect(result.size).to eq 3
+        3.times { |i|
+          expect(result[i].slice(:name, :file)).to eq expected[i]
+          expect(result[i].keys).to eq [:id, :name, :file]
+        }
       end
+    end
+  end
+
+  describe 'POST /api/v1/stocks' do
+    context '未ログインの場合' do
+    end
+    context 'ログイン済みの場合' do
+    end
+  end
+
+  describe 'GET /api/v1/stocks/:id' do
+    context '未ログインの場合' do
+    end
+    context 'ログイン済みの場合' do
+    end
+  end
+
+  describe 'PATCH /api/v1/stocks/:id' do
+    context '未ログインの場合' do
+    end
+    context 'ログイン済みの場合' do
+    end
+  end
+
+  describe 'DELETE /api/v1/stocks/:id' do
+    context '未ログインの場合' do
+    end
+    context 'ログイン済みの場合' do
     end
   end
 end

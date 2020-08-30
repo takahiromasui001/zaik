386b8f8e2df75ae8de4e65cf678251872a5368d8
Takahiro Masui <takahiro.masui@luxiar.com>
2020-08-30 18:06:41 +0900

    POST /api/v1/login

--- api/spec/requests/api/sessions_spec.rb
+++ api/spec/requests/api/sessions_spec.rb
@@ -4,23 +4,68 @@ RSpec.describe Api::V1::SessionsController, type: :request do
   describe 'POST /api/v1/login' do
     context 'ユーザ名 & パスワードが正しい場合' do
       it '200 OKを返すこと' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user', password: 'login_user_password' }
+        expect(response.status).to eq 200
       end
 
       it 'ログイン成功を表すメッセージを返すこと' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user', password: 'login_user_password' }
+        expect(JSON.parse(response.body)['message']).to eq 'login succeed'
       end
 
       it 'ログイン確認が成功すること' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user', password: 'login_user_password' }
+
+        get api_v1_logged_in_path
+        expect(response.status).to eq 200
+        expect(JSON.parse(response.body)['message']).to eq 'logged in'
       end
     end
 
-    context 'ユーザ名 & パスワードが誤っている場合' do
+    context 'ユーザ名が誤っている場合' do
       it '401 Unauthorizedを返すこと' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user_false', password: 'login_user_password' }
+        expect(response.status).to eq 401
       end
 
       it 'エラーメッセージを返すこと' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user_false', password: 'login_user_password' }
+        expect(JSON.parse(response.body)['message']).to eq 'ユーザ名またはパスワードに誤りがあります。'
       end
 
       it 'ログイン確認が失敗すること' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user_false', password: 'login_user_password' }
+
+        get api_v1_logged_in_path
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'パスワードが誤っている場合' do
+      it '401 Unauthorizedを返すこと' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user', password: 'login_user_password_false' }
+        expect(response.status).to eq 401
+      end
+
+      it 'エラーメッセージを返すこと' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user', password: 'login_user_password_false' }
+        expect(JSON.parse(response.body)['message']).to eq 'ユーザ名またはパスワードに誤りがあります。'
+      end
+
+      it 'ログイン確認が失敗すること' do
+        user = create(:user, name: 'login_user', password: 'login_user_password', password_confirmation: 'login_user_password')
+        post api_v1_login_path, params: { name: 'login_user', password: 'login_user_password_false' }
+
+        get api_v1_logged_in_path
+        expect(response.status).to eq 401
       end
     end
   end

87be88799d731f8581a5bae667ed61d29adec437
Takahiro Masui <takahiro.masui@luxiar.com>
2020-09-13 17:40:03 +0900

    テスト作成: 新規在庫の登録

--- client/src/__test__/domains/Stock/StockList/StockList.test.tsx
+++ client/src/__test__/domains/Stock/StockList/StockList.test.tsx
@@ -2,12 +2,35 @@ import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import axios from 'axios'
-
 import StockList from '../../../../domains/Stock/StockList'
+import { omit as _omit } from 'lodash'
 
 jest.mock('axios')
-jest.mock('react-redux')
+jest.mock('react-redux', () => {
+  return {
+    useDispatch: () => () => {
+      // do nothing
+    },
+    useSelector: () => {
+      return { errors: [], stockDetail: {} }
+    },
+  }
+})
 jest.mock('react-router-dom')
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
 
 describe('StockList', () => {
   test('StockListのレンダリング', async () => {
@@ -41,5 +64,59 @@ describe('StockList', () => {
         `http://localhost:3000/api/v1/stocks/?search=${searchWord}`
       )
     )
+  })
+
+  test('新規在庫の登録', async () => {
+    const openStockList = () => {
+      const myGetMock = jest.fn(() => Promise.resolve({ data: [] }))
+      axios.get.mockImplementation(myGetMock)
+      render(<StockList />)
+    }
+    const openCreateStockForm = () => {
+      const myStorehouseGetMock = jest.fn(() =>
+        Promise.resolve({ data: [{ name: 'store1', id: 1 }] })
+      )
+      axios.get.mockImplementationOnce(myStorehouseGetMock)
+
+      userEvent.click(screen.getByRole('img', { name: 'plus-circle' }))
+    }
+    const inputForm = async () => {
+      userEvent.type(screen.getByText('品名'), 'stock1')
+      userEvent.type(screen.getByText('色番号'), '0004')
+      userEvent.type(screen.getByText('製造年月日'), '2020-09-01')
+      userEvent.type(screen.getByText('残量'), '20')
+      userEvent.click(screen.getByLabelText('新品・中古'))
+      await waitFor(() => userEvent.click(screen.getByText('新品')))
+      userEvent.click(screen.getByLabelText('保管場所'))
+      await waitFor(() => userEvent.click(screen.getByText('store1')))
+    }
+
+    openStockList()
+    openCreateStockForm()
+    expect(screen.queryByText('在庫情報の新規作成')).toBeVisible()
+    await inputForm()
+
+    const myPostMock = jest.fn(() =>
+      Promise.resolve({ data: { name: 'stock1', id: 1 } })
+    )
+    axios.post.mockImplementation(myPostMock)
+    userEvent.click(screen.getByText('OK'))
+
+    await waitFor(() =>
+      myPostMock.mock.results[0].value.then((v) =>
+        expect(v.data).toEqual({ name: 'stock1', id: 1 })
+      )
+    )
+    expect(screen.queryByText('在庫情報の新規作成')).not.toBeVisible()
+    expect(_omit(myPostMock.mock.calls[0][1], ['manufacturingDate'])).toEqual({
+      colorNumber: '0004',
+      condition: 'unused',
+      name: 'stock1',
+      quantity: '20',
+      storehouse_id: 1,
+    })
+    expect(
+      myPostMock.mock.calls[0][1].manufacturingDate.isSame('2020-09-01')
+    ).toBe(true)
   })
 })
